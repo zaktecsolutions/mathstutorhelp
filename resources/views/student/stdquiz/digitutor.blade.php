d7581a3f5a6142eb456f2ac07d453ab93c50c91e
zaktecsolutions <30366846+zaktecsolutions@users.noreply.github.com>
2020-04-27 02:11:47 +0100

    27/04

--- resources/views/student/stdquiz/digitutor.blade.php
+++ resources/views/student/stdquiz/digitutor.blade.php
@@ -5,7 +5,7 @@
 <div class="row justify-content-center">
     <div class="col-md-9 col-sm-11 col-xs-12">
         <div class="card">
-            <div class="card-header">Name: {{ $user->name }}  DigiTutor
+            <div class="card-header">  <h4> {{ $user->name }} DigiTutor  </h4>
                 <a class="float-right" href="/studentdashboard">Back to dashboard</a> </div>
             {{-- <a href="blank.html" class="btn btn-primary">Add user</a> --}}
             <div class="card-body">
@@ -16,26 +16,81 @@
                             <th scope="col">Quiz Name</th>
                             <th scope="col">Quiz Percent</th>
                             <th scope="col">Quiz Grade</th>
-                            <th scope="col">Notes</th>
-                            <th scope="col">To Do</th>
-                        </tr>
-                    </thead> 
-                    <tbody>
-
                     
-                        <tr>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {{--   <tr>
                             <th scope="row">1</th>
                             <td>{{$result->quiz->quiz_name }}</td>
-                            <td>Percent : {{$result->quiz_percent}}</td>  
-                            <td>Grade : {{$result->grade}}</td>
-                                          
+                        <td>Percent : {{$result->quiz_percent}}</td>
+                        <td>Grade : {{$result->grade}}</td>
                         </tr>
-                        
+                        --}}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header"> <h4> {{ $user->name }} Notes  </h4>
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Lesson Name</th>
+                            <th scope="col">Lesson Description</th>
+                            <th scope="col">Status</th>
+                            <th scope="col"> Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {{-- @foreach($topic->lessons as $lesson)
+                        <tr>
+                            <th scope="row">{{ $lesson->lesson_code }}</th>
+                        <td>{{ $lesson->lesson_name }}</td>
+                        <td>{{ $lesson->lesson_desc }}</td>
+                        <td>{{$lesson->is_complete() ? 'Complete' : 'Pending'}}</td>
+                        <td> <a href="">
+                                <button type="button" class="btn btn-primary float-left">Lesson</button></a></td>
+                        </tr>
+                        @endforeach --}}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header"><h4> {{ $user->name }} ToDoList  </h4> 
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Lesson Name</th>
+                            <th scope="col">Lesson Description</th>
+                            <th scope="col">Status</th>
+                            <th scope="col"> Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                      {{--   @foreach($topic->lessons as $lesson)
+                        <tr>
+                            <th scope="row">{{ $lesson->lesson_code }}</th>
+                            <td>{{ $lesson->lesson_name }}</td>
+                            <td>{{ $lesson->lesson_desc }}</td>
+                            <td>{{$lesson->is_complete() ? 'Complete' : 'Pending'}}</td>
+                            <td> <a href="">
+                                    <button type="button" class="btn btn-primary float-left">Lesson</button></a></td>
+                        </tr>
+                        @endforeach --}}
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
 </div>
-{{-- </div> --}}
+
 @endsection
